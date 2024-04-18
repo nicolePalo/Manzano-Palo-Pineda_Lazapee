7a5c54a9ac8bd32093c21e0235129f7b0234a606
Nicole Palo <nclarissepalo@gmail.com>
2024-04-18 13:15:55 +0800

    updated

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'payroll_app/home.html')
+
+def create_employee(request):
+    pass
+
+def payslips(request):
+    pass
+
+def update_employee(request):
+    pass
+
+def view_payslip(request):
+    pass
