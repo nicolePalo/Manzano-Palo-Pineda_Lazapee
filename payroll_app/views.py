3935bc67cb93033bc0fc1421bc0769f5bf73ee03
Nicole Palo <nclarissepalo@gmail.com>
2024-04-18 13:28:52 +0800

    imported models into views.py
    next step: add querysets

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Employee, Payslip
 
 # Create your views here.
+'''
+All querysets done here.
+Creating Data Records: The django application receives the data from the form through a function in views.py and prepares it for inputting to the database. 
+'''
 
 def home(request):
     return render(request, 'payroll_app/home.html')
+# The above has been given as an example. 
 
 def create_employee(request):
     pass
