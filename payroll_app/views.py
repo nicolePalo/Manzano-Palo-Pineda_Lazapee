6ebbd9c41c9184b6093a0bfc4202268e3b9cf04a
Nicole Palo <nclarissepalo@gmail.com>
2024-04-29 16:17:51 +0800

    Created functions to generate payslips

--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Employee, Payslip
 
 # Create your views here.
@@ -25,17 +26,40 @@ def payslips(request):
         'nav_selected': 'Payslips',
         payslip:'payslips'
     }
-    return render(request, 'payroll_app/payslips.html', context)
+
+    if request.method == "POST":
+        employee = request.POST.get('payslip_employee')
+        month = request.POST.get('payslip_month')
+        year = request.POST.get('payslip_year')
+        cycle = request.POST.get('payslip_cycle')
+
+        if Payslip.objects.filter(employee=employee).exists():
+            messages.error(request, "Employee already has payslip.")
+            return render(request, 'payroll_app/payslips.html', context)
+        else: 
+            id_number = get_object_or_404(Employee,pk=employee)
+
+            # Cycle 1 payslip
+            Payslip.objects.create(
+                id_number=id_number,
+                month=month,
+                date_range='',
+                year=year,
+                cycle=1
+            )
+
+            #Cycle 2 payslip
+            Payslip.objects.create(
+                id_number=id_number,
+                cycle=2
+            )
+                
+    else: 
+        return render(request, 'payroll_app/payslips.html', context)
 
 def update_employee(request):
     pass
-
-def generate_payslip(request):
-    # generates two payslips 
-    if request.method == 'POST':
-        return render(request, 'payroll/payslips.html')
     
-def view_payslip(request):
-    return render(request,'payroll/view_payslip.html')
-
-    
+def view_payslip(request, pk):
+    payslip = get_object_or_404(Payslip,pk=pk)
+    return render(request,'payroll/view_payslip.html', {payslip:'payslip'})
